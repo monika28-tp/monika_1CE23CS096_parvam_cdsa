c09f4058fad2609464385fcebac818b4aa1d5f9f
monika28-tp <nagarathna908554@gmail.com>
2024-11-06 16:12:36 +0530

    Nov6

---
+++ global.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int a=5;
+    void add(){
+        printf("%d ",a);
+    }
+    void sub(){
+        a++;
+        printf("%d",a);
+    }
+    int main(){
+        add();
+        sub();
+}
